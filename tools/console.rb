ab09e4425a83875ab8fa70ef026c099e222557ca
Adriana <adriana.amaral@msn.com>
2021-03-02 11:44:55 -0600

    shane changes

--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,20 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
+l1 = Lifter.new("Adri", 50)
+l2 = Lifter.new("Shane", 100)
+l3 = Lifter.new("Calla", 20)
+l4 = Lifter.new("Theo", 200)
+
+g1 = Gym.new("strong people")
+g2 = Gym.new("no pain no gain")
+g3 = Gym.new("shane's club")
+
+m1 = Membership.new(20, g1, l2)
+m2 = Membership.new(50, g3, l3)
+m3 = Membership.new(30, g2, l1)
+m4 = Membership.new(30, g1, l4)
+
 binding.pry
 
 puts "Gains!"
